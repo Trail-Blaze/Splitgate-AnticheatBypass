38b5a91aae9a3711be20bbd119d8a598d1447517
Ender0001 <70291335+Ender-0001@users.noreply.github.com>
2021-07-24 19:52:07 -0400

    Add files via upload

---
+++ Splitgate-AnticheatBypass/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace Args_Dumper
+{
+    class Program
+    {
+
+        public static string GetArgs(string[] args)
+        {
+            string final = "";
+            foreach (string arg in args)
+            {
+                final += arg + " ";
+            }
+            return final;
+        }
+
+        public static bool ArgsInvalid(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+
+            if (ArgsInvalid(args))
+            {
+                Process.Start("steam://rungameid/677620");
+                Environment.Exit(0);
+            }
+            
+            var Splitgate = new Process();
+            Splitgate.StartInfo.FileName = Directory.GetCurrentDirectory() + "\\PortalWars\\Binaries\\Win64\\PortalWars-Win64-Shipping.exe";
+            Splitgate.StartInfo.Arguments = GetArgs(args);
+
+            Splitgate.Start();
+
+            Splitgate.WaitForExit();
+        }
+    }
+}
